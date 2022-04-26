ea837f72197cdc4a87ad6b8bcc7d65e9984bbf9d
zumesan <zumehashi3240518@icloud.com>
2022-04-26 19:56:18 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,8 @@
 class RoomsController < ApplicationController
 
+  def index
+  end
+
   def new
     @room = Room.new
   end
@@ -12,6 +15,7 @@ class RoomsController < ApplicationController
       render :new
     end
   end
+
 
   private
 
